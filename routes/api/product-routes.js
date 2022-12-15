d9b1348e26c13f132552ac4c39e780b07ee14231
Matt Lahey <mdlahey@outlook.com>
2022-12-15 16:08:20 -0500

    fixed small error in Product Tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -76,7 +76,7 @@ router.post('/', (req, res) => {
     stock: req.body.stock,
     category_id: req.body.category_id,
     tagIds: req.body.tagIds
-  })
+    })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
